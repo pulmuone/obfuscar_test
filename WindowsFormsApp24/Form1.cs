0791f866ea9d3d410ffd7f9a81c089bb3847bed7
hmji <hmji@nansoft.co.kr>
2023-08-16 16:05:43 +0900

    Add project files.

---
+++ WindowsFormsApp24/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp24
+{
+    public partial class Form1 : Form
+    {
+        public string UserName = "gwise";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClassLibrary1.Class1 class1 = new ClassLibrary1.Class1();
+
+            label1.Text = class1.UserID;
+
+            ClassLibrary2.Class1 class2 = new ClassLibrary2.Class1();
+
+            label2.Text = class2.UserID;
+        }
+    }
+}
